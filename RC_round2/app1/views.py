1d96d8102fd1e994cbd85ee8f87766f3f1ba3706
Vathsal Tammewar <vathsaltammewar@Vathsals-MacBook-Air.local>
2023-05-13 00:00:26 +0530

    final integration pending

--- RC_round2/app1/views.py
+++ RC_round2/app1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse, redirect
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import *
 from django.contrib.auth.models import User
